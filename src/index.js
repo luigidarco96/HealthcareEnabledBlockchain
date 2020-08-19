bce3077586954dccff6c27434cbed862d776ec92
spike322 <m.borrazzo@studenti.unisa.it>
2020-08-19 18:28:44 +0200

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,17 @@ function getAccount() {
     return myArgs[0];
 }
 
+function randomBetween(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+  }
+
 async function main() {
 
     // Set up web3 object, connected to the local development network
-    const web3 = new Web3('http://localhost:8545');
+    const web3 = new Web3('http://7fd7e66fb949.ngrok.io/');
+    //const web3 = new Web3('http://localhost:8545');
 
     // Set up a web3 contract, representing our deployed Box instance, using the contract loader
     const loader = setupLoader({ provider: web3 }).web3;
@@ -20,10 +27,14 @@ async function main() {
     const contract = loader.fromArtifact('PersonalInfo', contract_key);
 
     // Send a transaction to store() a new value in the Box
-    currentAccount = await getAccount();
+    currentAccount = randomBetween(0, 9);// await getAccount();
+    const spo2 = randomBetween(55, 110);
+    const bpm = randomBetween(50, 150);
     const accounts = await web3.eth.getAccounts();
-    await contract.methods.createRecord(100, 90)
+    await contract.methods.createRecord(spo2, bpm )
         .send({ from: accounts[currentAccount], gas: 1000000, gasPrice: 1e6 });
+    
+    console.log('{id: '+currentAccount+', spo2: '+spo2+', bpm: '+bpm+'}');
 }
 
 main();
